51f2b7d38ff1c8cecd4a1265319d8e770a0e2bca
PI860 <paulina.pyczot@gmail.com>
2024-12-16 07:46:04 +0100

    feat: new Parser for JSON file

---
+++ raytracer/src/main/java/rayrangers/raytracer/parser/quicktype/SceneParser.java
@@ -0,0 +1,12 @@
+package rayrangers.raytracer.parser.quicktype;
+
+/**
+ * @author Paulina Pyczot
+ * @date 16.12.2024
+ **/
+
+public class SceneParser {
+    public static void main(String[] args) {
+
+    }
+}
