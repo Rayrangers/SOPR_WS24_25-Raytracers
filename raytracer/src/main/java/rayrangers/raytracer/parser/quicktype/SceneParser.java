04e23784f73b8553439886bc92994996e86c643f
PI860 <paulina.pyczot@gmail.com>
2024-12-16 07:48:25 +0100

    feat [SCRUM-27]: new Parser for JSON file

---
+++ raytracer/src/main/java/rayrangers/raytracer/parser/quicktype/SceneParser.java
@@ -0,0 +1,14 @@
+package rayrangers.raytracer.parser.quicktype;
+
+/**
+ * Parser for a JSON file.
+ *
+ * Inspired by
+ * https://quicktype.io/
+ **/
+
+public class SceneParser {
+    public static void main(String[] args) {
+
+    }
+}
