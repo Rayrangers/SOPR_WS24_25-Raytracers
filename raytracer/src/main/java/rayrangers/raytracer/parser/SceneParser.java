2995885f5311be4f826fe662a9698d669cbbee18
PI860 <paulina.pyczot@gmail.com>
2024-12-18 18:23:07 +0100

    feat [SCRUM-27]: new SceneParser class added, SampleQuicktype class deleted

--- raytracer/src/main/java/rayrangers/raytracer/parser/SceneParser.java
+++ raytracer/src/main/java/rayrangers/raytracer/parser/SceneParser.java
@@ -1,106 +1,100 @@
 package rayrangers.raytracer.parser;
 
-/**
- * Parser for a JSON file.
- *
- * Inspired by
- * https://quicktype.io/
- **/
+import com.fasterxml.jackson.databind.ObjectMapper;
+import rayrangers.raytracer.world.Scene;
+import rayrangers.raytracer.parser.quicktype.*;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-
+import java.io.File;
 import java.io.IOException;
-import java.time.OffsetDateTime;
-import java.time.OffsetTime;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.temporal.ChronoField;
 
 public class SceneParser {
-    // Date-time helpers
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
-            .appendOptional(DateTimeFormatter.ISO_DATE_TIME)
-            .appendOptional(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-            .appendOptional(DateTimeFormatter.ISO_INSTANT)
-            .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SX"))
-            .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssX"))
-            .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-            .toFormatter()
-            .withZone(ZoneOffset.UTC);
+    /**
+     * Parses a JSON file into a Scene object.
+     *
+     * @param filePath the path to the JSON file
+     * @return a Scene object populated with data from the JSON file
+     * @throws IOException if there is an issue reading or parsing the file
+     */
+    public static Scene parseSceneFromJson(String filePath) throws IOException {
+        // Step 1: Read the JSON file into Quicktype-generated classes
+        SceneData sceneData = objectMapper.readValue(new File(filePath), SceneData.class);
 
-    public static OffsetDateTime parseDateTimeString(String str) {
-        return ZonedDateTime.from(SceneParser.DATE_TIME_FORMATTER.parse(str)).toOffsetDateTime();
+        // Step 2: Map the Quicktype classes to the custom Scene class
+        return mapToScene(sceneData);
     }
 
-    private static final DateTimeFormatter TIME_FORMATTER = new DateTimeFormatterBuilder()
-            .appendOptional(DateTimeFormatter.ISO_TIME)
-            .appendOptional(DateTimeFormatter.ISO_OFFSET_TIME)
-            .parseDefaulting(ChronoField.YEAR, 2020)
-            .parseDefaulting(ChronoField.MONTH_OF_YEAR, 1)
-            .parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
-            .toFormatter()
-            .withZone(ZoneOffset.UTC);
+    /**
+     * Saves a Scene object to a JSON file.
+     *
+     * @param scene the Scene object to save
+     * @param filePath the path to the JSON file
+     * @throws IOException if there is an issue writing the file
+     */
+    public static void saveSceneToJson(Scene scene, String filePath) throws IOException {
+        // Step 1: Map the custom Scene class to Quicktype-generated classes
+        SceneData sceneData = mapToSceneData(scene);
 
-    public static OffsetTime parseTimeString(String str) {
-        return ZonedDateTime.from(SceneParser.TIME_FORMATTER.parse(str)).toOffsetDateTime().toOffsetTime();
+        // Step 2: Write the Quicktype classes to a JSON file
+        objectMapper.writeValue(new File(filePath), sceneData);
     }
 
-//    // Serialize/deserialize helpers for SampleQuicktype class:
-//    public static SampleQuicktype fromJsonString(String json) throws IOException {
-//        return getObjectReader().readValue(json);
-//    }
-//
-//    public static String toJsonString(SampleQuicktype obj) throws JsonProcessingException {
-//        return getObjectWriter().writeValueAsString(obj);
-//    }
-
-//    // Serialize/deserialize helpers
-//    public static SceneData fromJsonString(String json) throws IOException {
-//        return getObjectReader().readValue(json);
-//    }
-//
-//    public static String toJsonString(SceneData obj) throws JsonProcessingException {
-//        return getObjectWriter().writeValueAsString(obj);
-//    }
-
-    private static ObjectReader reader;
-    private static ObjectWriter writer;
-
-    private static void instantiateMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.findAndRegisterModules();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(OffsetDateTime.class, new JsonDeserializer<OffsetDateTime>() {
-            @Override
-            public OffsetDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-                String value = jsonParser.getText();
-                return SceneParser.parseDateTimeString(value);
-            }
-        });
-        mapper.registerModule(module);
-        // Reader and writer for SampleQuicktype class:
-        //reader = mapper.readerFor(SampleQuicktype.class);
-        //writer = mapper.writerFor(SampleQuicktype.class);
-
-//        reader = mapper.readerFor(SceneData.class);
-//        writer = mapper.writerFor(SceneData.class);
+    /**
+     * Maps a SceneData object (from Quicktype) to a Scene object.
+     *
+     * @param sceneData the SceneData object to map
+     * @return a Scene object populated with data from the SceneData object
+     */
+    private static Scene mapToScene(SceneData sceneData) {
+        Scene scene = new Scene();
+        // Map data from SceneData to the custom Scene class
+        // Example (simplified): Set the camera from the first Cameraentity object
+        scene.setCamera(mapToCamera(sceneData.getCameraentity()[0]));
+        // Map additional attributes here...
+        return scene;
     }
 
-    private static ObjectReader getObjectReader() {
-        if (reader == null) instantiateMapper();
-        return reader;
+    /**
+     * Maps a Scene object to a SceneData object (for JSON serialization).
+     *
+     * @param scene the Scene object to map
+     * @return a SceneData object populated with data from the Scene object
+     */
+    private static SceneData mapToSceneData(Scene scene) {
+        SceneData sceneData = new SceneData();
+        // Map data from the custom Scene class to the Quicktype SceneData class
+        // Example (simplified): Convert `Camera` to `Cameraentity` and set it
+        sceneData.setCameraentity(new Cameraentity[]{ mapToCameraentity(scene.getCamera()) });
+        // Map additional attributes here...
+        return sceneData;
     }
 
-    private static ObjectWriter getObjectWriter() {
-        if (writer == null) instantiateMapper();
-        return writer;
+    /**
+     * Maps a custom Camera object to a Quicktype Cameraentity object.
+     *
+     * @param camera the custom Camera object to map
+     * @return a Cameraentity object populated with data from the Camera object
+     */
+    private static Cameraentity mapToCamera(rayrangers.raytracer.world.Camera camera) {
+        Cameraentity cameraEntity = new Cameraentity();
+        cameraEntity.setName(camera.getName());
+        cameraEntity.setUUID(camera.getUuid());
+        // Map additional attributes here...
+        return cameraEntity;
+    }
+
+    /**
+     * Maps a Quicktype Cameraentity object to a custom Camera object.
+     *
+     * @param cameraEntity the Cameraentity object to map
+     * @return a Camera object populated with data from the Cameraentity object
+     */
+    private static rayrangers.raytracer.world.Camera mapToCamera(Cameraentity cameraEntity) {
+        rayrangers.raytracer.world.Camera camera = new rayrangers.raytracer.world.Camera();
+        camera.setName(cameraEntity.getName());
+        camera.setUuid(cameraEntity.getUUID());
+        // Map additional attributes here...
+        return camera;
     }
 }
