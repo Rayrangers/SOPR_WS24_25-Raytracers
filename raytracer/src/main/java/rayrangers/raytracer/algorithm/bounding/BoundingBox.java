4ad5436f8bcd5c407f0d8efa1ab40dd8c5c19f1a
birkenbube <jan.birkert@tutanota.de>
2024-12-23 16:04:00 +0000

    fix: deal with edge case of negative direction or division by zero

--- raytracer/src/main/java/rayrangers/raytracer/algorithm/bounding/BoundingBox.java
+++ raytracer/src/main/java/rayrangers/raytracer/algorithm/bounding/BoundingBox.java
@@ -45,35 +45,41 @@ public class BoundingBox implements Hittable {
         double x2e = ray.getOrigin().getCoord(2);
         double x3e = ray.getOrigin().getCoord(3);
 
-        // Get x1, x2 and x3 coordinates of ray direction d
-        double x1d = ray.getDirection().getCoord(1);
-        double x2d = ray.getDirection().getCoord(2);
-        double x3d = ray.getDirection().getCoord(3);
+        // Get x1, x2 and x3 coordinates of ray direction d and calculate reciprocal
+        // Divisions by 0 are handled by IEEE floating point conventions (yields +/- Infinity)
+        double reciprocalX1d = 1 / ray.getDirection().getCoord(1);
+        double reciprocalX2d = 1 / ray.getDirection().getCoord(2);
+        double reciprocalX3d = 1 / ray.getDirection().getCoord(3);
 
-        double tx1min = (x1min - x1e) / x1d;
-        double tx1max = (x1max - x1e) / x1d;
-        double tx2min = (x2min - x2e) / x2d;
-        double tx2max = (x2max - x2e) / x2d;
-        double tx3min = (x3min - x3e) / x3d;
-        double tx3max = (x3max - x3e) / x3d;
-
-        // Check if swapping is necessary due to a negative ray direction
-        if (tx1min > tx1max) {
-            double tmp = tx1min;
-            tx1min = tx1max;
-            tx1max = tmp;
+        double tx1min;
+        double tx1max;
+        double tx2min;
+        double tx2max;
+        double tx3min;
+        double tx3max;
+        
+        if (reciprocalX1d >= 0) {
+            tx1min = (x1min - x1e) * reciprocalX1d;
+            tx1max = (x1max - x1e) * reciprocalX1d;
+        } else {
+            tx1max = (x1min - x1e) * reciprocalX1d;
+            tx1min = (x1max - x1e) * reciprocalX1d;
         }
 
-        if (tx2min > tx2max) {
-            double tmp = tx2min;
-            tx2min = tx2max;
-            tx2max = tmp;
+        if (reciprocalX2d >= 0) {
+            tx2min = (x2min - x2e) * reciprocalX2d;
+            tx2max = (x2max - x2e) * reciprocalX2d;
+        } else {
+            tx2max = (x2min - x2e) * reciprocalX2d;
+            tx2min = (x2max - x2e) * reciprocalX2d;
         }
 
-        if (tx3min > tx3max) {
-            double tmp = tx3min;
-            tx3min = tx3max;
-            tx3max = tmp;
+        if (reciprocalX3d >= 0) {
+            tx3min = (x3min - x3e) * reciprocalX3d;
+            tx3max = (x3max - x3e) * reciprocalX3d;
+        } else {
+            tx3max = (x3min - x3e) * reciprocalX3d;
+            tx3min = (x3max - x3e) * reciprocalX3d;
         }
 
         // Check all permutations
