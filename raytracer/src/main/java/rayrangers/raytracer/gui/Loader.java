77d27fb6d7eb7fc4b961c042de9026b5c85a0a62
adi9113 <adrian.desiderato.ad@gmail.com>
2025-01-06 18:19:53 +0000

    revert: remove some of my changes (nonsense)

--- raytracer/src/main/java/rayrangers/raytracer/gui/Loader.java
+++ raytracer/src/main/java/rayrangers/raytracer/gui/Loader.java
@@ -1,11 +1,5 @@
 package rayrangers.raytracer.gui;
 
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-
 import io.qt.core.QFile;
 import io.qt.widgets.QGraphicsPixmapItem;
 import io.qt.widgets.QGraphicsScene;
@@ -16,15 +10,7 @@ import io.qt.widgets.QStackedWidget;
 import io.qt.widgets.QToolButton;
 import io.qt.widgets.QWidget;
 import io.qt.widgets.tools.QUiLoader;
-import rayrangers.raytracer.algorithm.Renderer;
-import rayrangers.raytracer.math.TrafoMatrix;
-import rayrangers.raytracer.math.Vertex3D;
-import rayrangers.raytracer.parser.ObjParser;
-import rayrangers.raytracer.view.ViewPane;
-import rayrangers.raytracer.world.Camera;
-import rayrangers.raytracer.world.Entity;
-import rayrangers.raytracer.world.LightSource;
-import rayrangers.raytracer.world.Scene;
+import rayrangers.raytracer.Prototype;
 import io.qt.core.QTimer;
 import io.qt.gui.QPixmap;
 import io.qt.widgets.QProgressBar;
@@ -80,7 +66,6 @@ public class Loader extends QMainWindow {
         });
 
 
-
         // Load progress bar
         progressBar = centralWidget.findChild(QProgressBar.class, "ProgressBar_main");
         
@@ -96,7 +81,7 @@ public class Loader extends QMainWindow {
                 new Thread(() -> {
                     System.out.println("Starting Prototype main");
                     try {
-                        runPrototype();
+                        Prototype.main(new String[]{});
                         System.out.println("Prototype main finished");
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -126,46 +111,5 @@ public class Loader extends QMainWindow {
         if (progressValue >= 100) {
             timer.stop();
         }
-    }
-
-    private static void runPrototype() throws Exception {
-        Scene scene = new Scene(Color.BLACK);
-        Camera camera = new Camera(new Vertex3D(0, 25, 400), 0, 0, 0, 75, 100, 2000, 2000);
-        ViewPane viewPane = camera.getViewPane();
-
-        scene.addCamera(camera);
-        Entity teapot = ObjParser.parseObjFile("examples/teapot/Teapot.obj");
-        Entity tuna = ObjParser.parseObjFile("examples/tuna/tuna-low.obj");
-
-        TrafoMatrix tmTea = new TrafoMatrix(-50, -100, 10, -90, 10, -33, 1, 1, 1);
-        teapot.transform(tmTea);
-
-        TrafoMatrix tmTuna = new TrafoMatrix(0, 150, 0, -90, 0, 0, 1, 1, 1);
-        tuna.transform(tmTuna);
-
-        scene.addEntity(teapot);
-        scene.addEntity(tuna);
-
-        LightSource lightSource1 = new LightSource(0.15, new Vertex3D(0, 50, 200), Color.WHITE);
-        scene.addLightSource(lightSource1);
-
-        Renderer renderer = new Renderer(scene, camera.getUuid());
-        renderer.render();
-
-        BufferedImage bufferedImage = new BufferedImage(viewPane.getResX(), viewPane.getResY(),
-                BufferedImage.TYPE_INT_RGB);
-        for (int j = 0; j < viewPane.getResY(); j++) {
-            for (int i = 0; i < viewPane.getResX(); i++) {
-                bufferedImage.setRGB(i, j, viewPane.getPixelAt(i, j).getColor().getRGB());
-            }
-        }
-
-        try {
-            File output = new File("artifacts/prototype.png");
-            ImageIO.write(bufferedImage, "png", output);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println();
     }
 }
