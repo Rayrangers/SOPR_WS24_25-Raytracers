e20800686b81750515368f224dadc1f2d20caf3b
David <gerida01@thu.de>
2025-01-21 18:50:10 +0000

    resolve merge confict

--- raytracer/src/main/java/rayrangers/raytracer/Prototype.java
+++ raytracer/src/main/java/rayrangers/raytracer/Prototype.java
@@ -28,25 +28,56 @@ public class Prototype {
         long start = System.currentTimeMillis();
 
         Scene scene = new Scene(Color.BLACK);
+<<<<<<< HEAD
         // Camera camera = new Camera(new Vertex3D(0, 25, 400), 0, 0, 0, 75, 100, 2000, 2000);
         Camera camera = new Camera(new Vertex3D(400, 25, 0), 0, 90, 0, 75, 100, 2000, 2000);
+=======
+        Camera camera = new Camera(new Vertex3D(400, 50, 0), 0, 90, 0, 75, 100, 2000, 2000);
+        // Camera camera = new Camera(new Vertex3D(0, 300, 0), -90, 180, 0, 50, 100, 1000, 1000);
+>>>>>>> 5ce7e00 (feat: Shadows + Blinn)
         ViewPane viewPane = camera.getViewPane();
 
         scene.addCamera(camera);
-        Entity teapot = ObjParser.parseObjFile("examples/teapot/Teapot.obj");
-        Entity tuna = ObjParser.parseObjFile("examples/tuna/tuna-low.obj");
+        //Entity teapot = ObjParser.parseObjFile("examples/teapot/Teapot.obj");
+        //Entity tuna = ObjParser.parseObjFile("examples/tuna/tuna-low.obj");
+        //Entity water = ObjParser.parseObjFile("examples/water-molecule/water-molecule.obj");
+        Entity teapotChess = ObjParser.parseObjFile("examples/teapot-chess/teapotonchess.obj");
 
-        TrafoMatrix tmTea = new TrafoMatrix(-50, -100, 10, -90, 10, -33, 1, 1, 1);
-        teapot.transform(tmTea);
+        TrafoMatrix tmTea = new TrafoMatrix(-50, -100, 10, 0, 0, 0, 1, 1, 1);
+        teapotChess.transform(tmTea);
 
-        TrafoMatrix tmTuna = new TrafoMatrix(0, 150, 0, -90, 0, 0, 1, 1, 1);
-        tuna.transform(tmTuna);
+        //TrafoMatrix tmTea = new TrafoMatrix(-50, -100, 10, -90, 10, -33, 1, 1, 1);
+        //teapot.transform(tmTea);
 
+        //TrafoMatrix tmTuna = new TrafoMatrix(80, 70, 100, -90, 0, 0, 1, 1, 1);
+        //tuna.transform(tmTuna);
+
+<<<<<<< HEAD
         scene.addEntity(teapot);
         scene.addEntity(tuna);
 
         LightSource lightSource1 = new LightSource(0.15, new Vertex3D(300, 250, 200), Color.WHITE);
         LightSource lightSource2 = new LightSource(0.15, new Vertex3D(300, 50, 0), Color.WHITE);
+=======
+        // List<Vertex3D> vlist = new ArrayList<>();
+        // vlist.add(new Vertex3D(100, 0, 0));
+        // vlist.add(new Vertex3D(0, 100, 0));
+        // vlist.add(new Vertex3D(0, 0, 0));
+
+        // Face f = new Triangle(null, null, vlist);
+
+        // List<Face> faces = new ArrayList<>();
+        // faces.add(f);
+
+        // Entity triangleEnt = new Entity(null, faces, vlist);
+        // scene.addEntity(triangleEnt);
+
+        //scene.addEntity(teapot);
+        //scene.addEntity(tuna);
+        scene.addEntity(teapotChess);
+
+        LightSource lightSource1 = new LightSource(0.15, new Vertex3D(300, 250, 150), Color.WHITE);
+>>>>>>> 5ce7e00 (feat: Shadows + Blinn)
         scene.addLightSource(lightSource1);
         scene.addLightSource(lightSource2);
 
