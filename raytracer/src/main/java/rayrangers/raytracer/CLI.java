ff760b6e7be9aaa5a2c23056903ae3559f690dcd
birkenbube <jan.birkert@tutanota.de>
2025-01-22 18:19:22 +0000

    feat: add cli class

---
+++ raytracer/src/main/java/rayrangers/raytracer/CLI.java
@@ -0,0 +1,80 @@
+package rayrangers.raytracer;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
+
+import rayrangers.raytracer.algorithm.Renderer;
+import rayrangers.raytracer.parser.SceneParser;
+import rayrangers.raytracer.view.ViewPane;
+import rayrangers.raytracer.world.Scene;
+
+/**
+ * Prototype of the raytracer utilizing the JSON parser for the scene
+ * specification based on Quicktype.
+ */
+public class CLI {
+
+    /**
+     * Main method.
+     * 
+     * @param args string arguments
+     */
+    public static void main(String[] args) {
+        String scenePath = null;
+        String outputPath = null;
+        if (args.length == 0) {
+            System.err.println("Error: No arguments provided. Please specify the scene path and output path.");
+            System.exit(1);
+        }
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-path") && i + 1 < args.length) {
+            scenePath = args[i + 1];
+            i++;
+            } else if (args[i].equals("-output") && i + 1 < args.length) {
+            outputPath = args[i + 1];
+            i++;
+            }
+        }
+        long start = System.currentTimeMillis();
+
+        Scene scene = null;
+        UUID cameraUUID = null;
+        try {
+            scene = SceneParser.parseScene(scenePath);
+            cameraUUID = scene.getCameras().keySet().iterator().next();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (scene != null && cameraUUID != null) {
+
+            ViewPane viewPane = scene.getCameras().get(cameraUUID).getViewPane();
+
+            Renderer renderer = new Renderer(scene, cameraUUID);
+            renderer.render();
+
+            long end = System.currentTimeMillis();
+            System.out.printf("Total runtime for rendering: %f s%n", (end - start) / 1000.0);
+
+            BufferedImage bufferedImage = new BufferedImage(viewPane.getResX(), viewPane.getResY(),
+                    BufferedImage.TYPE_INT_RGB);
+            for (int j = 0; j < viewPane.getResY(); j++) {
+                for (int i = 0; i < viewPane.getResX(); i++) {
+                    bufferedImage.setRGB(i, j, viewPane.getPixelAt(i, j).getColor().getRGB());
+                }
+            }
+
+            try {
+                File output = new File(outputPath);
+                ImageIO.write(bufferedImage, "png", output);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println();
+        }
+    }
+}
